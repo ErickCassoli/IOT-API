954a56c5850b5889254e4931181bad11fcdce62a
ErickCassoli <erick.mcassoli@gmail.com>
2023-12-14 19:45:22 -0300

    feat: fixed the models sensordata

--- hydrosonar/models.py
+++ hydrosonar/models.py
@@ -4,19 +4,8 @@ import pytz
 
 class SensorData(models.Model):
     value = models.FloatField()
-    timestamp = models.DateTimeField()
-
-    def save(self, *args, **kwargs):
-        # Define o fuso horário desejado (por exemplo, 'America/Sao_Paulo')
-        tz = pytz.timezone('America/Sao_Paulo')
-
-        # Obtém a data e hora atuais no fuso horário desejado
-        timestamp_with_timezone = timezone.now().astimezone(tz)
-
-        # Atribui o timestamp ao objeto SensorData
-        self.timestamp = timestamp_with_timezone
-        super().save(*args, **kwargs)
-
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
 class ProcessedData(models.Model):
     sensor_data = models.OneToOneField(SensorData, on_delete=models.CASCADE, primary_key=True)
     volume_liters = models.FloatField()
